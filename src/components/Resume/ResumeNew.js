10c5bd0ec63422b9996d8d6c0887172e33c6fd2e
Felix Odete <felix.odette@gmail.com>
2022-10-18 13:34:15 +0300

    Update: Resume link

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink = 'https://raw.githubusercontent.com/felixdette/felix-odete/master/src/Assets/Felix_Odete___Resume.pdf';
+const resumeLink = 'https://github.com/felixodette/felix-odete/blob/master/src/Assets/Felix_Odete___Resume.pdf';
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
