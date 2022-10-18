f32af95789177d33de2d0a2bc3306816ae264059
Felix Odete <felix.odette@gmail.com>
2022-10-18 14:12:07 +0300

    Update: Logo and repo fork link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -112,7 +112,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/felixodette/felix-odete"
                 target="_blank"
                 className="fork-btn-inner"
               >
